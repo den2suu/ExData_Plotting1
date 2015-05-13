f790f980438b939c2061e7e3477835ba47fac050
den2suu <den2suu@users.noreply.github.com>
2015-05-13 19:37:14 +0900

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@ library(dplyr)
 dt <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
 ## lubridating of Date
 dt$Date <- dmy(dt$Date)
-## extracting the date 2007-02-01 and 2007-01-02
+## extracting the date 2007-02-01 and 2007-02-02
 exdt <- dt %>% filter(as.Date(Date) == as.Date("2007-02-01") | as.Date(Date) == as.Date("2007-02-02"))
 ## integrating Date and Time 
 datetime <- as.POSIXlt(paste(exdt$Date, exdt$Time), tz = "America/Los_Angeles", format = "%Y-%m-%d %H:%M:%S")
